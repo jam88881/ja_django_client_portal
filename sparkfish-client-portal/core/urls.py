b217ead7ca828f8652566fc21669dfadf7a36fbb
John Amarante <jamarante@sparkfish.com>
2020-06-18 13:21:12 -0700

    dashboard commit

---
+++ sparkfish-client-portal/core/urls.py
@@ -0,0 +1,17 @@
+# -*- encoding: utf-8 -*-
+"""
+License: MIT
+Copyright (c) 2019 - present AppSeed.us
+"""
+
+from django.contrib import admin
+from django.urls import path, include  # add this
+from django.views.generic import TemplateView
+
+urlpatterns = [
+    path('', TemplateView.as_view(template_name="index.html")),
+    path('admin/', admin.site.urls),
+    path("", include("authentication.urls")),  # add this
+    path('accounts/', include("allauth.urls")),
+    path("", include("app.urls"))  # add this
+]
