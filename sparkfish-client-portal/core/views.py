f988a4d36f926e72e231712e730d82b69cd6ea9d
John Amarante <jamarante@sparkfish.com>
2020-06-22 11:22:53 -0700

    removed api key

--- sparkfish-client-portal/core/views.py
+++ sparkfish-client-portal/core/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 import requests
 
 def dash(request):
-    response = requests.get('https://api.trello.com/1/boards/B5t1aUPH/cards/?key=4e15254bd0830e2b1f4e79d12bebf60d&token=6d46576c490239c0e0ba711c703b4daee24243e3485ab393381ba649a1f11272')
+    response = requests.get('')
 
     boradData = response.json()
 
