13f86902a1fd49f58ac7b74d0bbbc2de0fefe3ad
John Amarante <jamarante@sparkfish.com>
2020-06-23 14:36:53 -0700

    removed key

--- sparkfish-client-portal/core/views.py
+++ sparkfish-client-portal/core/views.py
@@ -4,7 +4,7 @@ import requests
 from datetime import date, timedelta
 
 def dash(request):
-    response = requests.get('https://api.trello.com/1/boards/B5t1aUPH/cards/?key=4e15254bd0830e2b1f4e79d12bebf60d&token=6d46576c490239c0e0ba711c703b4daee24243e3485ab393381ba649a1f11272')
+    response = requests.get('')
 
     boardData = response.json()
 
